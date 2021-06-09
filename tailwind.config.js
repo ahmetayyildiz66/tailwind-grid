44019e9e24a620043abe952c3326dc837ccfebd4
ahmetayyildiz66 <ahmetayyildiz66@gmail.com>
2021-06-09 18:33:48 +0300

    Defined custom grid rows and columns in configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        'wrapper': '1000px'
+      },
+      gridTemplateRows: {
+        'layout-row': '100px 200px 400px 100px'
+      },
+      gridTemplateColumns: {
+        'layout-col': 'repeat(3, 1fr) 120px'
+      }
+    },
   },
   variants: {
     extend: {},
